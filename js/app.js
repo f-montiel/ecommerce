cc1402053ca7da99b2166d9a567fa26795e1d85f
Facundo Montiel <montiel.facu@gmail.com>
2022-09-04 01:58:26 -0300

    Pagina de inicio con Lottie y carga de la marca

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let lottie = document.getElementById("lottie");
+let body = document.getElementById("body");
+let timeOut = setTimeout(() => {
+    mostrarBody()
+}, 5000);
+function mostrarBody(){
+    document.querySelector("body").removeChild(lottie);
+    body.classList.remove("d-none");
+    clearTimeout(timeOut);
+}
+let marca = document.getElementById("marca");
+let str = marca.innerHTML;
+marca.innerHTML = "";
+let speed = 500;
+let i = 0;
+function typeWriter(){
+    if(i<str.length){
+        marca.innerHTML += str.charAt(i);
+        i++;
+        setTimeout(typeWriter, speed)
+}
+}
+setTimeout(typeWriter, speed);
