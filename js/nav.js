980cf513be99e88937013d833d0a0a6976e3e09c
Facundo Montiel <montiel.facu@gmail.com>
2022-09-08 21:07:55 -0300

    Logica del Login, cerrar sesión y marcas de los productos creados en el dropdown de marcas

--- js/nav.js
+++ js/nav.js
@@ -4,46 +4,133 @@ let iconoUsuario = document.getElementById("iconoUsuario");
 let iconoCarrito = document.getElementById("iconoCarrito");
 let emailInputFormModal = document.getElementById("emailInputFormModal");
 let passwordModal = document.getElementById("passwordModal");
+let btnCerrarSecion = document.getElementById("btnCerrarSecion");
+btnCerrarSecion.addEventListener("click", cerrarSecion);
 const modalLogin = new bootstrap.Modal(document.getElementById("modalLogin"));
-
-let usuarioLogueado =
-  JSON.parse(localStorage.getItem("listaUsuariosAdministradoresKey")) || {};
+document.getElementById("modalLogin").addEventListener("hidden.bs.modal", limpiarFormulario);
+const formularioLogin = document.getElementById("formularioLogin");
+let usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
+let productos = JSON.parse(localStorage.getItem("listaProductosKey")) || [];
+let marcas = [];
+productos.forEach(producto => {
+    marcas.push(producto.marca);
+});
+let marcasUnicas = [...new Set(marcas)];
+marcasUnicas.forEach(marca => {
+  document.getElementById("marcas").innerHTML += `
+  <li><a class="dropdown-item" href="#">${marca}</a></li>
+  `  
+});
 
 let usuarios = [
   { email: "montiel.facu@gmail.com", password: "1234Facu", admin: false },
   { email: "jochydaoua@gmail.com", password: "123456Jo", admin: true },
   { email: "victoriapalaciof@gmail.com", password: "123Vicky", admin: true },
   { email: "eliseo.espindola1@gmail.com", password: "12345678", admin: true },
-  { email: "julianlopez@gmail.com", password: "12345678", admin: false },
+  { email: "julianlopez@gmail.com", password: "12345678", admin: false }
 ];
 
-btnIngresar.addEventListener("click", ingresar);
+formularioLogin.addEventListener("submit", ingresar);
 
-function guardarUsuariosEnLocalStorage() {
-  localStorage.setItem(
-    "listaUsuariosAdministradoresKey",
-    JSON.stringify(usuarioLogueado)
-  );
-}
 
-function ingresar() {
+function ingresar(e){
+  e.preventDefault();
   usuarios.forEach((usuario) => {
     if (
       emailInputFormModal.value === usuario.email &&
       passwordModal.value === usuario.password
-    ) {
-      usuarioLogueado = usuario;
-      btnLogin.classList.add("d-none");
-      iconoUsuario.classList.remove("d-none");
-      guardarUsuariosEnLocalStorage();
-      modalLogin.hide();
-    }
-    if (usuarioLogueado.admin === true) {
-      btnLogin.classList.add("d-none");
-      iconoCarrito.classList.add("d-none");
-    } else {
-      btnLogin.classList.add("d-none");
-      iconoCarrito.classList.remove("d-none");
-    }
-  });
+      ) {
+        usuarioLogueado = usuario;
+        guardarUsuariosEnLocalStorage();
+        chequearUsuario();
+        modalLogin.hide();
+      } else {
+        Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Los datos ingresados no son validos',
+        })
+        limpiarFormulario();
+      }
+    })
+  }
+
+function guardarUsuariosEnLocalStorage() {
+  localStorage.setItem("usuarioLogueado", JSON.stringify(usuarioLogueado));
 }
+
+function chequearUsuario(){
+  if(usuarioLogueado){
+    ocultarBotonLogin();
+    mostrarIconoCarrito();
+    mostrarIconoUsuario();
+    mostrarBotonCerrarSecion();
+  }
+}
+
+function ocultarBotonLogin(){
+  usuarioLogueado = localStorage.getItem("usuarioLogueado");
+  if(usuarioLogueado){
+    btnLogin.classList.add("d-none");
+  } else{
+    btnLogin.classList.remove("d-none");
+  }
+}
+function mostrarIconoUsuario(){
+  usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
+  if(usuarioLogueado){
+    iconoUsuario.classList.remove("d-none");
+    console.log(usuarioLogueado);
+    if(usuarioLogueado.admin){
+      console.log("El Usuario es admin")
+      //Si el usuario es admin
+      if(window.location.pathname == "/"){
+        // Si la pagina es el inicio
+        iconoUsuario.setAttribute("href", "/pages/admin.html");
+      } else {
+        // si estoy en cualquiera de las otras paginas
+        iconoUsuario.setAttribute("href", "../pages/admin.html");
+      }
+    } else {
+      console.log("El usuario no es admin");
+      // si el usuario no es admin.
+      if(window.location.pathname == "/"){
+        // Si la pagina es el inicio
+        iconoUsuario.setAttribute("href", "/pages/error.html");
+      } else {
+        iconoUsuario.setAttribute("href", "../pages/error.html");
+      }
+    }
+  } else{
+    iconoUsuario.classList.add("d-none");
+  }
+}
+function mostrarIconoCarrito(){
+  usuarioLogueado = JSON.parse(localStorage.getItem("usuarioLogueado"));
+  if(usuarioLogueado){
+    if(!usuarioLogueado.admin){iconoCarrito.classList.remove("d-none");}
+  } else{
+    iconoCarrito.classList.add("d-none");
+  }
+}
+function mostrarBotonCerrarSecion(){
+  if(usuarioLogueado){
+    btnCerrarSecion.classList.remove("d-none");
+  } else{
+    btnCerrarSecion.classList.add("d-none");
+  }
+}
+function cerrarSecion(){
+  localStorage.removeItem("usuarioLogueado");
+  chequearUsuario();
+}
+function limpiarFormulario(){
+  formularioLogin.reset();
+  emailInputFormModal.classList.remove("is-valid");
+  emailInputFormModal.classList.remove("is-invalid");
+  passwordModal.classList.remove("is-valid");
+  passwordModal.classList.remove("is-invalid");
+}
+
+
+chequearUsuario();
